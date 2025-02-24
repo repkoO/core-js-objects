b5029a8104577d2a2bba0a5538281517c5194712
Kirill Medvedev <repkomail@gmail.com>
2025-02-24 22:11:01 +0200

    feat: implement rectangle task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -182,8 +182,17 @@ function sellTickets(/* queue */) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  const rectangle = {
+    width,
+    height,
+
+    getArea() {
+      return this.width * this.heigth;
+    },
+  };
+
+  return rectangle;
 }
 
 /**
